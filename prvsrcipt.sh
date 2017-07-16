3c76209a9f57b7df5176b85ed024b6e5cf12bd34
gzmud <gzimud@gmail.com>
2017-07-16 21:52:30 +0800

    Update prvsrcipt.sh

--- prvsrcipt.sh
+++ prvsrcipt.sh
@@ -8,7 +8,7 @@ private script
 
 Usage:
 
-wget --cache=off https://raw.githubusercontent.com/gzmud/prvscript/master/prvsrcipt.sh -O prvsrcipt.sh && chmod +x prvsrcipt.sh 
+wget --cache=off --no-cache https://raw.githubusercontent.com/gzmud/prvscript/master/prvsrcipt.sh -O prvsrcipt.sh && chmod +x prvsrcipt.sh 
 . prvscript.sh
 EOF
 }
