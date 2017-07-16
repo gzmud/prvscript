6ca221a25bf6189b13595cb333df59a0b01c5d67
gzmud <gzmud@db9>
2017-07-17 05:52:37 +0800

    add lede sdk function

--- prvscript.sh
+++ prvscript.sh
@@ -96,3 +96,81 @@ function lede_pmakeimg()
 {
 	proxychains make image PACKAGES="$ledepkg"
 }
+function lede_setsdk()
+{
+
+# cat <<EOF >>feeds.conf.default
+# src-git shadowsocks-libev https://github.com/shadowsocks/openwrt-shadowsocks.git
+# src-git luci-app-shadowsocks https://github.com/shadowsocks/luci-app-shadowsocks.git
+# src-git ChinaDNS https://github.com/aa65535/openwrt-chinadns.git
+# src-git openwrt-dist-luci https://github.com/aa65535/openwrt-dist-luci.git
+# src-git DNS-forwarder https://github.com/aa65535/openwrt-dns-forwarder.git
+# src-git simple-obfs https://github.com/aa65535/openwrt-simple-obfs.git
+# EOF
+
+lede_getsource
+./scripts/feeds update -a
+}
+
+function lede_getsource()
+{
+#lede_getss
+# 添加 feeds
+git clone https://github.com/shadowsocks/openwrt-feeds.git package/feeds
+# 获取 shadowsocks-libev Makefile
+git clone https://github.com/shadowsocks/openwrt-shadowsocks.git package/shadowsocks-libev
+# 选择要编译的包 Network -> shadowsocks-libev
+#make menuconfig
+# 开始编译
+#make package/shadowsocks-libev/compile V=99
+
+# lede_getchinadns
+# 获取 Makefile
+git clone https://github.com/aa65535/openwrt-chinadns.git package/chinadns
+# 选择要编译的包 Network -> ChinaDNS
+# make menuconfig
+# # 开始编译
+# make package/chinadns/compile V=99
+
+# lede_getDNS-forwarder
+# 获取 Makefile
+git clone https://github.com/aa65535/openwrt-dns-forwarder.git package/dns-forwarder
+# 选择要编译的包 Network -> dns-forwarder
+# make menuconfig
+# # 开始编译
+# make package/dns-forwarder/compile V=99
+
+# lede_getsimple-obfs
+# 添加 feeds
+git clone https://github.com/shadowsocks/openwrt-feeds.git package/feeds
+# 获取 simple-obfs Makefile
+git clone https://github.com/aa65535/openwrt-simple-obfs.git package/simple-obfs
+# # 选择要编译的包 Network -> simple-obfs
+# make menuconfig
+# # 开始编译
+# make package/simple-obfs/compile V=99
+
+# lede_getluci-app-shadowsocks
+# Clone 项目
+git clone https://github.com/shadowsocks/luci-app-shadowsocks.git package/luci-app-shadowsocks
+# 编译 po2lmo (如果有po2lmo可跳过)
+# pushd package/luci-app-shadowsocks/tools/po2lmo
+# make && sudo make install
+# popd
+# # 选择要编译的包 LuCI -> 3. Applications
+# make menuconfig
+# # 开始编译
+# make package/luci-app-shadowsocks/compile V=99
+
+# lede_getopenwrt-dist-luci
+git clone https://github.com/aa65535/openwrt-dist-luci.git package/openwrt-dist-luci
+# 编译 po2lmo (如果有po2lmo可跳过)
+# pushd package/openwrt-dist-luci/tools/po2lmo
+# make && sudo make install
+# popd
+# # 选择要编译的包 LuCI -> 3. Applications
+# make menuconfig
+# # 开始编译
+# make package/openwrt-dist-luci/compile V=99
+}
+
