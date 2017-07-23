fceae2f80cedac8a9e11de65972c34e072e99821
gzmud <gzmud@db9>
2017-07-24 01:09:39 +0800

    add docker install function

--- prvscript.sh
+++ prvscript.sh
@@ -471,3 +471,30 @@ git pull
 make menuconfig
 make defconfig
 }
+
+funciton db_instdocker()
+{
+if [ `id -u` -eq 0 ];then  
+    echo "root ! go on"  
+else  
+    echo "not root ! exit"  
+	exit -1
+fi
+apt-get remove docker docker-engine docker.io
+apt-get update
+apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+	 
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce
+}
