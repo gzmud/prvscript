e0a95afbca9171512d5f7b2c6ecf5fe81f257af4
gzmud <gzimud@gmail.com>
2018-01-28 19:13:35 +0800

    Create ldcmd2.sh

---
+++ ldcmd2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#lede cmd ref  https://lede-project.org/docs/user-guide/imagebuilder
+#
+
+function prv_help ()
+{
+cat << EOF
+private script 
+
+Usage:
+
+wget --cache=off --no-cache https://raw.github.com/gzmud/prvscript/master/ledecmd.sh -O ldcmd2.sh && . ldcmd2.sh
+
+EOF
+}
